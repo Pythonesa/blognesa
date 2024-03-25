404ef0fd457319d1b89e7c7b2d075f378678faba
Pythonesa <pythonesamar@gmail.com>
2024-03-25 00:12:13 -0300

    :lipstick: post content width

--- src/components/Blog/PostContent.tsx
+++ src/components/Blog/PostContent.tsx
@@ -8,8 +8,8 @@ interface IPostContentProps {
 
 export default function PostContent({ content, children }: IPostContentProps) {
     return (
-        <div className="mx-auto max-w-prose mt-4">
-            <div className="aspect-w-3 aspect-h-2">
+        <div className="mx-auto mt-4">
+            <div className="aspect-w-3 aspect-h-2 max-w-prose mx-auto">
                 <img
                     className="object-cover object-center h-full rounded-lg m-auto"
                     src={content.imgSrc}
@@ -17,7 +17,7 @@ export default function PostContent({ content, children }: IPostContentProps) {
                     loading="lazy"
                 />
             </div>
-            <div className="prose prose-invert mt-8 prose-img:rounded-lg text-justify">
+            <div className="mt-8 prose-img:rounded-lg text-justify">
                 {children}
             </div>
         </div>

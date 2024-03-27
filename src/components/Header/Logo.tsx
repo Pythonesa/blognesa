0be09065f28c9e85baa2ecf60566e7f84b2acdb1
Pythonesa <pythonesamar@gmail.com>
2024-03-27 06:24:20 -0300

    :lipstick: fix images sizes

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -9,7 +9,7 @@ interface ILogoProps {
 export default function Logo({ icon, name, imgalt }: ILogoProps) {
     return (
         <div className="flex gap-1 items-center text-xl font-bold">
-            <img src={icon} alt={imgalt} />
+            <img src={icon} alt={imgalt} loading="lazy" className="w-16 h-16" />
             <GradiantText>{name}</GradiantText>
         </div>
     )
--- src/components/Hero/HeroSocial.tsx
+++ src/components/Hero/HeroSocial.tsx
@@ -6,7 +6,7 @@ interface IHeroSocialProps {
 export default function HeroSocial({ src, alt }: IHeroSocialProps) {
     return (
         <img 
-            className="w-16 h-16 hover:translate-y-1 transition-all" 
+            className="w-12 h-12 hover:translate-y-1 transition-all" 
             src={src} 
             alt={alt} 
             loading="lazy"

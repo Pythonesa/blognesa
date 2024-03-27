ce2e12387a58058f8ee2008fc40ad0ebdfc34615
Pythonesa <pythonesamar@gmail.com>
2024-03-27 06:01:12 -0300

    :bug: fix logo text

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -3,12 +3,13 @@ import GradiantText from "@/components/GradiantText";
 interface ILogoProps {
     icon: string;
     name: string;
+    imgalt: string;
 };
 
-export default function Logo({ icon, name }: ILogoProps) {
+export default function Logo({ icon, name, imgalt }: ILogoProps) {
     return (
         <div className="flex gap-1 items-center text-xl font-bold">
-            <img src={icon} alt={name} />
+            <img src={icon} alt={imgalt} />
             <GradiantText>{name}</GradiantText>
         </div>
     )

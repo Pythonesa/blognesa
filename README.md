555b7ecad47712508dfc5621428e6b378dfb0d95
Pythonesa <pythonesamar@gmail.com>
2024-03-27 03:51:52 -0300

    :sparkles: add series logic

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Blognesa
 
 Blog hecho con Astro, React y Tailwind.
+Usa Markdown para generar los posts.
---
+++ src/components/Series/SerieCard.tsx
@@ -0,0 +1,30 @@
+interface ISerieCardProps {
+    title: string;
+    description: string;
+    link: string;
+    imgSrc: string;
+    imgAlt: string;
+}
+
+export default function BlogCard(serie: ISerieCardProps) {
+    return (
+        <a className="hover:translate-y-1 transition-all" href={serie.link}>
+            <div className="overflow-hidden rounded-md bg-slate-800 text-center">
+                <div className="aspect-w-3 aspect-h-2">
+                    <img className="mx-auto"
+                        src={serie.imgSrc}
+                        alt={serie.imgAlt}
+                        loading="lazy"
+                    />
+                </div>
+
+                <div className="px-3 pt-4 pb-6">
+                    <h2 className="text-xl font-semibold">{serie.title}</h2>
+                    <div className="mt-2 text-sm">
+                        {serie.description}
+                    </div>
+                </div>
+            </div>
+        </a>
+    )
+}
